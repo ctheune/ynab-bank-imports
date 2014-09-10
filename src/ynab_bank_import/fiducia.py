947972873710dcbed8071a1ef40a82c1a0d3e44c
Christian Theune <ct@gocept.com>
2014-09-10 14:06:08 +0200

    PEP 8 cleanup for the new fiducia format

--- src/ynab_bank_import/fiducia.py
+++ src/ynab_bank_import/fiducia.py
@@ -15,16 +15,17 @@ class Dialect(csv.Dialect):
     lineterminator = '\n'
 
 
-def import_account(filename, ynab):   
+def import_account(filename, ynab):
     ## Skipping first lines with unneeded information
     with open(filename, newline='', encoding='ISO-8859-15') as f:
         bank_file = f.readlines()[12:]
-           
-    for record in csv.DictReader(bank_file, dialect=Dialect):       
+
+    for record in csv.DictReader(bank_file, dialect=Dialect):
         # Skipping last lines "Anfangssaldo" and "Endsaldo"
-        if record['Währung'] == "Anfangssaldo" or record['Währung'] == "Endsaldo":
+        if (record['Währung'] == "Anfangssaldo" or
+                record['Währung'] == "Endsaldo"):
             continue
-               
+
         t = ynab.new_transaction()
         t.Date = record['Buchungstag']
         t.Payee = record['Empfänger/Zahlungspflichtiger']
@@ -32,11 +33,11 @@ def import_account(filename, ynab):
 
         amount = decimal.Decimal(
             record['Umsatz'].replace('.', '').replace(',', '.'))
-        
+
         # Last column indicates postive / negative amount
         if record[' '] == 'S':
             t.Outflow = amount
         else:
             t.Inflow = amount
 
-        ynab.record_transaction(t) 
+        ynab.record_transaction(t)
